913bf4b35eab5fcfc481b35f8bc7f0e34f4f0050
Lowouis <louisguritapro@gmail.com>
2022-10-26 08:32:54 +0200

    tabs adds

--- perks.php
+++ perks.php
@@ -1,55 +1,26 @@
-<?php
-$languages = ["Javascript", "Java", "Python", "PHP", "C", "HTML", "CSS"];
-$library = ["bootstrap", "vanilla-tilt", "barbara", "numpy"];
-$framework = ["Twig"];
-?>
 <section id="perks" class="cat">
     <h1><a class="c-number">03.</a>Perks</h1>
-    <div id="perks-wrapper">
-        <table>
-            <tbody>
-            <tr>
-                <td>Languages</td>
-                <td>Libraries</td>
-                <td>Framework</td>
-            </tr>
-            <tr>
-                <td>Javascript</td>
-                <td>Bootstrap</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>Java</td>
-                <td>Vanila-tilt</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>Python</td>
-                <td>Barbara</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>PHP</td>
-                <td>Numpy</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>C</td>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>HTML</td>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>CSS</td>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-            </tbody>
-        </table>
-
-    </div>
+    <div class="d-flex align-items-start">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Java</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Javascript</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">C</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">PHP</button>
+            </li>
+            <li>
+                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Python</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+        </div>
 </section>
--- works.php
+++ works.php
@@ -1,27 +1,56 @@
 <section id="works" class="article-menu cat">
     <h1><a class="c-number">02.</a>Works</h1>
-    <div id="navbar-works" class="grid-1-4">
-        <nav>
-            <a class="star ab fade transparent-border">A* Algorithm</a>
-            <a class="nim ab fade transparent-border">Nim Game</a>
-            <a class="bw ab fade transparent-border">Boat War</a>
-            <a class="morpion ab fade transparent-border">Morpion IA</a>
-        </nav>
-        <div id="switcha" class="layers">
-            <article class="star content">
-                <h2>A* Solver Algorithm</h2>
-                <h3>2021</h3>
-                <p>Worked with a team of three designers to build a marketing website and e-commerce
+        <div class="d-flex align-items-start">
+            <div class="nav nav-fill flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="horizontal">
+                <button class=" nav-link nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Algo A*</button>
+                <button class=" nav-link nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Jeux de Nim IA</button>
+                <button class=" nav-link nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                <button class=" nav-link nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+            </div>
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active text-light" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                    <h2>A* Solver Algorithm</h2>
+                    <h3>2021</h3>
+                    <p>Worked with a team of three designers to build a marketing website and e-commerce
                     platform for blistabloc, an ambitious startup originating from Northeastern
                     Helped solidify a brand direction for blistabloc that spans both packaging and web
                     Interfaced with clients on a weekly basis, providing technological expertise
-                </p>
-                <div class="add">
-                    <a  href="https://github.com/Lowouis/ricochet-robot" target=”_blank” class="fade btn hover">Watch on github</a>
-                    <a target=”_blank” href="data/files/astarreport_fr.pdf" class="btn fade hover"> PDF Report</a>
+                    </p>
+                    <div class="add">
+                        <a  href="https://github.com/Lowouis/ricochet-robot" target=”_blank” class="fade btn hover">Watch on github</a>
+                        <a target=”_blank” href="data/files/astarreport_fr.pdf" class="btn fade hover"> PDF Report</a>
+                    </div>
                 </div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    <h2>Jeux de Nim IA</h2>
+                    <h3>2019</h3>
+                    <p> Worked with a team of three designers to build a marketing website and e-commerce
+                        platform for blistabloc, an ambitious startup originating from Northeastern
+                        Helped solidify a brand direction for blistabloc that spans both packaging and web
+                        Interfaced with clients on a weekly basis, providing technological expertise
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                        <h2>Morpion IA</h2>
+                        <h3>2021</h3>
+                        <p>
+                            Worked with a team of three designers to build a marketing website and e-commerce
+                            platform for blistabloc, an ambitious startup originating from Northeastern
+                            Helped solidify a brand direction for blistabloc that spans both packaging and web
+                            Interfaced with clients on a weekly basis, providing technological expertise
+                        </p>
 
-            </article>
-
+                </div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                    <h2>Boat War Java App</h2>
+                    <h3>2021</h3>
+                    <p>
+                        Worked with a team of three designers to build a marketing website and e-commerce
+                        platform for blistabloc, an ambitious startup originating from Northeastern
+                        Helped solidify a brand direction for blistabloc that spans both packaging and web
+                        Interfaced with clients on a weekly basis, providing technological expertise
+                    </p>
+                </div>
+            </div>
         </div>
 </section>
